d7620abd3b2929ef6bfce6875e2fed02c5ea24cd
Andy Shao <syscheme@hotmail.com>
2019-02-25 23:55:41 +0800

    updated usage

--- xtee.cc
+++ xtee.cc
@@ -23,25 +23,28 @@ void usage()
   std::cout << "Usage: xtee {-n|[-a] <file>} [-s <bps>] [-k <bytes>] [-t <secs>] [-d <secs>] [-q <secs>]" EOL
             << "       xtee [-c <cmdline>] [-l <TARGET>:<SOURCE>]" EOL
             << "Options:" EOL
-            << "  -v <level>           verbose level, default 4 to output progress onto PSTDERR(stdioPipes)" EOL
+            << "  -v <level>           verbose level, default 4 to output progress onto stderr" EOL
             << "  -a                   append to the output file" EOL
-            << "  -n                   no output file in addition to PSTDOUT(stdioPipes)" EOL
-            << "  -s <bps>             limit the transfer bitrate at beginning of reading from PSTDIN(stdioPipes) in bps" EOL
-            << "  -k <bytes>           skip a certain number of bytes at reading from PSTDIN(stdioPipes)" EOL
-            << "  -t <secs>            skip the given seconds of data read from PSTDIN(stdioPipes)" EOL
+            << "  -n                   no output file other than stdout" EOL
+            << "  -s <bps>             limits the transfer bitrate at reading from stdin in bps" EOL
+            << "  -k <bytes>           skips a certain amount of bytes at the beginning of reading from stdin" EOL
+            << "  -t <secs>            skips the given seconds of data at reading from stdin" EOL
             << "  -d <secs>            duration in seconds to run" EOL
-            << "  -q <secs>            timeout in seconds when no more data can be read from PSTDIN(stdioPipes)" EOL
+            << "  -q <secs>            timeout in seconds when no more data can be read from stdin" EOL
             << "  -c <cmdline>         the child command line to execute" EOL
-            << "  -l <TARGET>:<SOURCE> link the source fd to the target fd, <TARGET> or <SOURCE> is in format of" EOL
-            << "                        \"[<cmdId>.]<fd>\", where <cmdId> is the sequence number of -c options, and" EOL
-            << "                        default '0.' refers to the xtee command itself" EOL
+            << "  -l <TARGET>:<SOURCE> links the source fd to the target fd, <TARGET> or <SOURCE> is in format of" EOL
+            << "                       \"[<cmdNo>.]<fd>\", where <cmdNo> is the sequence number of -c options, and" EOL
+            << "                       default '0.' refers to the xtee command itself" EOL
             << "  -h                   display this screen" EOL
             << "Examples:" EOL
-            << "  a) the following command results same as runing ls -l | sort and ls -l | grep txt，but the outputs" EOL
-            << "      of only one round of ls -l is taken by both sort and grep" EOL
+            << "  a) the following command results the same as runing \"ls -l | sort and ls -l | grep txt\"，but the" EOL
+            << "     outputs of the single round of \"ls -l\" will be taken by both \"sort\" and \"grep\" commands:" EOL
             << "       xtee -c \"ls -l\" -c \"sort\" -c \"grep txt\" -l 1.1:2.0 -l 1.1:3.0" EOL
-            << "  b) the following command downloads from a web at a limited speed of 3.75Mbps, zip and save as a file" EOL
-            << "       xtee -c \"wget - http://…\" -c \"zip - -o file.zip\" -l 0:1.1 -l 2.0:1 -n -s 3750000" EOL
+            << "  b) the following equal commands download from a web at a limited speed of 3.75Mbps, zip and save" EOL
+            << "     as a file:" EOL
+            << "       xtee -c \"wget -O - http://…\" -c \"zip - -o file.zip\" -l 0:1.1 -l 2.0:1 -n -s 3750000" EOL
+            << "       wget -O - http://… | xtee -c \"zip - -o file.zip\" -l 2.0:1 -n -s 3750000" EOL
+            << "       wget -O - http://… | xtee -n -s 3750000 | zip - -o file.zip" EOL
             << EOL;
 }
 
